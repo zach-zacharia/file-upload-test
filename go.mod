9ab6248a14fb2f3f5e684f5cd86302fb58c22f84
Zach Zacharia <dzakilando9696@gmail.com>
2024-08-16 11:21:20 +0700

    working on binwalk

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module file-upload
 
 go 1.22.6
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/fatih/color v1.17.0
+	github.com/gin-gonic/gin v1.10.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
@@ -18,6 +21,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,12 +19,12 @@ type FileType struct {
 	Extension string   `json:"extension"`
 	Content   string   `json:"content"`
 	Strings   []string `json:"strings"`
-	Contains  []string `json:"contains"`
 }
 
 // FileTypes represents the collection of file types
-type FileTypes struct {
-	FileTypes []FileType `json:"file_types"`
+type Rules struct {
+	FileTypes      []FileType `json:"file_types"`
+	ForbiddenFiles []string   `json:"forbidden_files"`
 }
 
 func main() {
@@ -38,12 +39,13 @@ func main() {
 	server.POST("/upload", func(c *gin.Context) {
 		file, fileHeader, err := c.Request.FormFile("file")
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			response := gin.H{"message": err.Error()}
+			c.JSON(http.StatusBadRequest, response)
 			return
 		}
 
 		fileExt := filepath.Ext(fileHeader.Filename)
-		fileinfo, filestrings, err := extensionCheck()
+		fileinfo, filestrings, forbiddenFiles, err := extensionCheck()
 		if err != nil {
 			response := gin.H{"message": err.Error()}
 			c.JSON(http.StatusInternalServerError, response)
@@ -53,8 +55,9 @@ func main() {
 		// Extension check
 		contentDescription, ok := fileinfo[fileExt]
 		if !ok {
+			color.Red("\nForbidden file extension found!")
 			response := gin.H{"message": "File extension is not allowed"}
-			c.JSON(http.StatusBadRequest, response)
+			c.JSON(http.StatusForbidden, response)
 			return
 		}
 
@@ -84,8 +87,9 @@ func main() {
 		}
 
 		if !containsContent(string(output), contentDescription) {
-			response := gin.H{"message": "File content does not match the expected type"}
-			c.JSON(http.StatusBadRequest, response)
+			color.Red("\nFile description inconsistency found!")
+			response := gin.H{"message": "File description does not match the expected type"}
+			c.JSON(http.StatusForbidden, response)
 			return
 		}
 
@@ -99,14 +103,30 @@ func main() {
 		}
 
 		if !containsAnyString(string(output), filestrings[fileExt]) {
+			color.Red("\nIrregular strings found inside file!")
 			response := gin.H{"message": "Irregular strings in file detected"}
-			c.JSON(http.StatusBadRequest, response)
+			c.JSON(http.StatusForbidden, response)
 			return
 		}
 
-		// Image check using 'binwalk'
-		// images := []string{".jpg", ".jpeg", ".png", ".gif"}
-		// if tempFile.Name()
+		// Checking hidden files using binwalk
+		cmd = exec.Command("binwalk", tempFile.Name())
+		output, err = cmd.Output()
+		if err != nil {
+			response := gin.H{"message": err.Error()}
+			c.JSON(http.StatusInternalServerError, response)
+			return
+		}
+
+		file_list := hiddenFileCheck(string(output))
+		fmt.Print(file_list)
+
+		if containsAnyString(file_list, forbiddenFiles) {
+			color.Red("Forbidden hidden files found!")
+			response := gin.H{"message": "Forbidden hidden files detected"}
+			c.JSON(http.StatusForbidden, response)
+			return
+		}
 
 		// Move the file to the final destination
 		finalPath := "./user_files/" + fileHeader.Filename
@@ -117,6 +137,7 @@ func main() {
 			return
 		}
 
+		color.Green("\nNo anomaly found, uploading file...")
 		response := gin.H{"message": "File uploaded successfully"}
 
 		c.JSON(http.StatusOK, response)
@@ -126,37 +147,35 @@ func main() {
 }
 
 // extensionCheck reads the JSON file and returns a map of file extensions to content descriptions
-func extensionCheck() (map[string]string, map[string][]string, error) {
+func extensionCheck() (map[string]string, map[string][]string, []string, error) {
 	data, err := os.ReadFile("rules.json")
 	if err != nil {
-		return nil, nil, fmt.Errorf("error reading file: %v", err)
+		return nil, nil, nil, fmt.Errorf("error reading file: %v", err)
 	}
 
-	var fileTypes FileTypes
-	err = json.Unmarshal(data, &fileTypes)
+	var rules Rules
+	err = json.Unmarshal(data, &rules)
 	if err != nil {
-		return nil, nil, fmt.Errorf("error parsing JSON file: %v", err)
+		return nil, nil, nil, fmt.Errorf("error parsing JSON file: %v", err)
 	}
 
-	extMap := make(map[string]string)
-	for _, ft := range fileTypes.FileTypes {
-		extMap[ft.Extension] = ft.Content
+	contentMap := make(map[string]string)
+	for _, ft := range rules.FileTypes {
+		contentMap[ft.Extension] = ft.Content
 	}
 
-	strMap := make(map[string][]string)
-	for _, ft := range fileTypes.FileTypes {
-		strMap[ft.Extension] = ft.Strings
+	stringMap := make(map[string][]string)
+	for _, ft := range rules.FileTypes {
+		stringMap[ft.Extension] = ft.Strings
 	}
 
-	conMap := make(map[string][]string)
-	for _, ft := range fileTypes.FileTypes {
-		conMap[ft.Extension] = ft.Contains
-	}
+	forbiddenFiles := rules.ForbiddenFiles
 
-	return extMap, strMap, nil
+	return contentMap, stringMap, forbiddenFiles, nil
 }
 
 func containsContent(output, contentDescription string) bool {
+
 	fmt.Printf("Command output: %s\n", output)                           // Debug print
 	fmt.Printf("Expected content description: %s\n", contentDescription) // Debug print
 
@@ -180,37 +199,30 @@ func normalizeContentDescription(content string) string {
 	return strings.ToLower(strings.TrimSpace(content))
 }
 
-func hiddenFileCheck(input []string, allowed_files []string) bool {
-	return true
+func hiddenFileCheck(output string) string {
+	lines := strings.Split(output, "\n")
+
+	// Iterate through lines to process each
+	for _, line := range lines {
+		// Skip header and separator lines
+		if strings.HasPrefix(line, "DECIMAL") || strings.HasPrefix(line, "------") {
+			continue
+		}
+
+		// Extract the description part
+		fields := strings.Fields(line)
+		if len(fields) < 3 {
+			continue // Skip lines that don't have enough fields
+		}
+
+		// Join all parts after the second field to form the description
+		description := strings.Join(fields[2:], " ")
+
+		// Extract the first sentence before the comma
+		firstSentence := strings.SplitN(description, ",", 2)[0]
+		firstSentence = strings.TrimSpace(firstSentence)
+
+		return firstSentence
+	}
+	return ""
 }
-
-// func hiddenFileCheck(output string) []string {
-// 	lines := strings.Split(output, "\n")
-
-// 	// Iterate through lines to process each
-// 	for _, line := range lines {
-// 		// Skip header and separator lines
-// 		if strings.HasPrefix(line, "DECIMAL") || strings.HasPrefix(line, "------") {
-// 			continue
-// 		}
-
-// 		// Extract the description part
-// 		fields := strings.Fields(line)
-// 		if len(fields) < 3 {
-// 			continue // Skip lines that don't have enough fields
-// 		}
-
-// 		// Join all parts after the second field to form the description
-// 		description := strings.Join(fields[2:], " ")
-
-// 		// Extract the first sentence before the comma
-// 		firstSentence := strings.SplitN(description, ",", 2)[0]
-// 		firstSentence = strings.TrimSpace(firstSentence)
-// 		lines := strings.Split(firstSentence, "\n")
-
-// 		var fileList []string
-// 		fileList = append(fileList, lines...)
-// 		return fileList
-// 	}
-// 	return nil
-// }
